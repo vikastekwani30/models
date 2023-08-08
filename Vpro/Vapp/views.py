6bc0594d5dc892f24f52ac0a703148b7cb280cfd
vikastekwani30 <139840364+vikastekwani30@users.noreply.github.com>
2023-08-08 11:22:34 +0530

    Add files via upload

---
+++ Vpro/Vapp/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from .models import  Article
+
+# Create your views here.
+
+def article_list(request):
+    article = Article.objects.all().order_by('date')
+
+    return render(request,'articles/article_list.html',{'articles':article})
